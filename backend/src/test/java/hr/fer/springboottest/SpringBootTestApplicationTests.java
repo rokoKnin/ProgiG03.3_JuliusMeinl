f23c25f1e50e1ae3a437bf04c881ec053e75c6e6
Roko Tojcic <datathing0@gmail.com>
2025-10-21 15:12:49 +0200

    pokusavam namjestit springBoot

---
+++ backend/src/test/java/hr/fer/springboottest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.hr.fer.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
