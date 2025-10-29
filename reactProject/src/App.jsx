3588cd23e46898620138a7dcbca5f385ca3e5ec5
GraciaHlobik <gracia.hlobik@fer.hr>
2025-10-29 10:18:40 +0100

    test gita

--- reactProject/src/App.jsx
+++ reactProject/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './index.css'
+function Home() {
+  return(<div className='desno'><h1>Home Page</h1></div> );
 }
 
-export default App
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Page</h1>;
+}
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+      {/* Navigation */}
+      <nav>
+        <div className="cont">
+        <div>
+        <Link to="/">Home</Link> </div>
+        <div>
+        <Link to="/about">About</Link> </div>
+        <div>
+        <Link to="/contact">Contact</Link>
+        </div>
+        </div>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
