455064fe3355df316fe5bbb5093f8370b57518c9
GraciaHlobik <gracia.hlobik@fer.hr>
2025-11-02 13:07:47 +0100

    implementacija rutinga i google mapsa

--- reactProject/src/App.jsx
+++ reactProject/src/App.jsx
@@ -1,109 +1,25 @@
 
-import * as React from 'react';
-import { 
-    AppBar, 
-    Button,
-    Box, 
-    Toolbar, 
-    IconButton, 
-    Typography,
-    Menu,
-    MenuItem
-} from '@mui/material';
-
-import MenuIcon from '@mui/icons-material/Menu';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+import HomePage from "./HomePage.jsx";
+import LogIn from "./LogIn.jsx";
+import About from "./About.jsx";
+import Contact from "./Contact.jsx";
+import Header from './layouts/Header.jsx';
 
-function Home() {
-  return(<div className='desno'><h1>Home Page</h1></div> );
-import { HashRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./HomePage.jsx"
-import LogIn from "./LogIn.jsx"
-
-function App() {
+export default function App() {
 
   return (
     <HashRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/logIn" element={<LogIn/>}/>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/logIn" element={<LogIn/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
     </HashRouter>
-  )
-}
-
-function About() {
-  return <h1>About Page</h1>;
-}
-
-function Contact() {
-  return <h1>Contact Page</h1>;
-}
-
-function Routing() {
-  return (
-    
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
   );
 }
 
-export default function App() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-             aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-          >
-                        <MenuIcon />
-                      </IconButton>
-
-                  <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        slotProps={{
-          list: {
-            'aria-labelledby': 'basic-button',
-          },
-        }}
-      >
-        <MenuItem onClick={handleClose}>Home</MenuItem>
-        <MenuItem onClick={handleClose}>About</MenuItem>
-        <MenuItem onClick={handleClose}>Contact</MenuItem>
-      </Menu>
-
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
--- reactProject/src/Contact.jsx
+++ reactProject/src/Contact.jsx
@@ -1,21 +1,19 @@
-
 import * as React from 'react';
 import './index.css';
 import CustomMap from "./CustomMap";
 import { APIProvider } from "@vis.gl/react-google-maps";
-
-function HomePage() {
+function Contact() {
   return (
     <main style={{ padding: 16 }}>
-      <h2>Dobrodošli u Blue sun hotel</h2>
+      <h2>kontaktirajte nas</h2>
+      <p>ili posjetite na ovoj lokaciji:</p>
       <div className="mapa">
       <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
         <CustomMap />
       </APIProvider>
     </div>
     </main>
-    
   );
 }
 
-export default HomePage;
+export default Contact;
