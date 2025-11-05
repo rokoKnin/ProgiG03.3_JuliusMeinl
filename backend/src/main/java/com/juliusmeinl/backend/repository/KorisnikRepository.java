c98a2ea32b2c8f129ecfb1e30926bec0237aead4
Nino Ljubas <nino.ljubas@gmail.com>
2025-11-05 20:29:02 +0100

    dodan WebConfig i promijenjen KorisnikRepo

--- backend/src/main/java/com/juliusmeinl/backend/repository/KorisnikRepository.java
+++ backend/src/main/java/com/juliusmeinl/backend/repository/KorisnikRepository.java
@@ -1,4 +1,11 @@
 package com.juliusmeinl.backend.repository;
 
-public class KorisnikRepository {
+
+import com.juliusmeinl.backend.model.Korisnik;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface KorisnikRepository extends JpaRepository<Korisnik, Integer> {
+    Optional<Korisnik> findByEmail(String email);  //moram preko email provjerit jel postoji korisnik u bazi
 }
