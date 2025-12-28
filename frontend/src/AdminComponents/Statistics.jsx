fddd887d823cbf21137b184ef136ed1935d11c97
Andrija Rebic <ar54850@fer.hr>
2025-12-28 19:03:18 +0100

    Statistics page - dodavanje okvira  za grafove

--- frontend/src/AdminComponents/Statistics.jsx
+++ frontend/src/AdminComponents/Statistics.jsx
@@ -1,8 +1,15 @@
 export default function Statistics() {
     return (
-        <div>
+        /*<div>
             <h2>Statistika</h2>
             <p>Ovdje će ići statistički podaci o korisnicima, rezervacijama i slično.</p>
+        </div>*/
+        <div className="Statistics" style= {{display: "flex", flexWrap: "wrap", gap: "1rem", justifyContent: "space-between"}}>
+            <div className="dataCard" style={{width: "100%", height: "20rem", background: "white"}}>Graf 1</div>
+            <div className="dataCard" style={{width: "45%", height: "20rem", background: "white"}}>Graf 2</div>
+            <div className="dataCard" style={{width: "45%", height: "20rem", background: "white"}}>Graf 3</div>
+            <div className="dataCard" style={{width: "45%", height: "20rem", background: "white"}}>Graf 4</div>
+            <div className="dataCard" style={{width: "45%", height: "20rem", background: "white"}}>Graf 5</div>
         </div>
     );
 }
