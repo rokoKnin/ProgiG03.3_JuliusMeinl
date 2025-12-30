1d4572fb9ecc8f2f1be09fcefacee13758290c91
Nino Ljubas <nino.ljubas@gmail.com>
2025-12-30 22:41:37 +0100

    dodana funkcionalnost broja dostupnih soba

--- backend/src/main/java/com/juliusmeinl/backend/controller/RezervirajSobuController.java
+++ backend/src/main/java/com/juliusmeinl/backend/controller/RezervirajSobuController.java
@@ -1,26 +1,32 @@
 package com.juliusmeinl.backend.controller;
 
 import com.juliusmeinl.backend.model.Soba;
+import com.juliusmeinl.backend.service.RezervirajSobuService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("api/room-reservation")
 public class RezervirajSobuController {
+    private final RezervirajSobuService rezervirajSobuService;
 
-    @PostMapping
-    public List<Soba> dostupneSobePoFilter(@RequestBody Map<String, LocalDate> dateMap) {
-        //provjeri rezervirajSobu, ako datum odgovara, uzmi sobaId i onda provjeri po njemu u entitetu soba koja je vrsta sobe
-        // za svaku vrstu napravi counter za tu vrstu
+    public RezervirajSobuController(RezervirajSobuService rezervirajSobuService) {
+        this.rezervirajSobuService = rezervirajSobuService;
+    }
 
+    @PostMapping("/available")
+    public List<Map<String, Object>> dostupneSobe(@RequestBody Map<String, String> dateMap) {
+        // moram napravit counter za svaku vrstu sobe mogucu
+        LocalDate datumOd = LocalDate.parse(dateMap.get("datumOd"));
+        LocalDate datumDo = LocalDate.parse(dateMap.get("datumDo"));
 
-
-        return null;
+        return rezervirajSobuService.dostupneSobe(datumOd, datumDo);
     }
 }
--- backend/src/main/java/com/juliusmeinl/backend/service/RezervirajSobuService.java
+++ backend/src/main/java/com/juliusmeinl/backend/service/RezervirajSobuService.java
@@ -1,7 +1,51 @@
 package com.juliusmeinl.backend.service;
 
+import com.juliusmeinl.backend.model.Soba;
+import com.juliusmeinl.backend.model.VrstaSobe;
+import com.juliusmeinl.backend.repository.RezervirajSobuRepository;
+import com.juliusmeinl.backend.repository.SobaRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class RezervirajSobuService {
+    private final SobaRepository sobaRepository;
+    private final RezervirajSobuRepository rezervirajSobuRepository;
+
+    public RezervirajSobuService(SobaRepository sobaRepository, RezervirajSobuRepository rezervirajSobuRepository) {
+        this.sobaRepository = sobaRepository;
+        this.rezervirajSobuRepository = rezervirajSobuRepository;
+    }
+
+
+    public List<Map<String, Object>> dostupneSobe(LocalDate datumOd, LocalDate datumDo) {
+
+        List<Integer> zauzeteIds = rezervirajSobuRepository.findNedostupneSobeById(datumOd, datumDo);
+
+        if (zauzeteIds.isEmpty()) {
+            zauzeteIds = List.of(-1); // ako mi vrati prazan, stavljam id -1 koji nema sig da mi ne pukne kod
+        }
+        List<Object[]> rezultat = sobaRepository.countDostupneSobePoVrsti(zauzeteIds);
+
+        List<Map<String, Object>> response = new ArrayList<>();
+
+        for (Object[] r : rezultat) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("vrsta", r[0]);
+            map.put("balkon", r[1]);
+            map.put("pogledNaMore", r[2]);
+            map.put("brojDostupnih", r[3]);
+
+            response.add(map);
+        }
+        return response;
+    }
+
+
 }
