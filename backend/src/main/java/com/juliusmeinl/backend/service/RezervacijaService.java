da60fe1d0e2498f607f036add4121555a9b3d0ed
Nino Ljubas <nino.ljubas@gmail.com>
2026-01-13 18:34:31 +0100

    azuriranje klasa i koda

--- backend/src/main/java/com/juliusmeinl/backend/service/RezervacijaService.java
+++ backend/src/main/java/com/juliusmeinl/backend/service/RezervacijaService.java
@@ -31,28 +31,6 @@ public class RezervacijaService {
     }
 
 
-    public List<Map<String, Object>> dostupneSobe(LocalDate datumOd, LocalDate datumDo) {
-
-        List<Integer> zauzeteIds = rezervirajSobuRepository.findNedostupneSobeById(datumOd, datumDo);
-
-        if (zauzeteIds.isEmpty()) {
-            zauzeteIds = List.of(-1); // ako mi vrati prazan, stavljam id -1 koji nema sig da mi ne pukne kod
-        }
-        List<Object[]> rezultat = sobaRepository.countDostupneSobePoVrsti(zauzeteIds);
-
-        List<Map<String, Object>> response = new ArrayList<>();
-
-        for (Object[] r : rezultat) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("vrsta", r[0]);
-            map.put("balkon", r[1]);
-            map.put("pogledNaMore", r[2]);
-            map.put("brojDostupnih", r[3]);
-
-            response.add(map);
-        }
-        return response;
-    }
 
     public Integer kreirajRezervaciju(Integer id) {
         Optional<Korisnik> korisnik = korisnikRepository.findById(id);
