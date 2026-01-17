dc9ac12a774d1bf4874f5d870162e52077870165
GraciaHlobik <gracia.hlobik@fer.hr>
2026-01-17 12:48:55 +0100

    payment dodana ograničenja za slanje podataka s kartice, popravci na dashboardu

--- backend/src/main/java/com/juliusmeinl/backend/model/Drzava.java
+++ backend/src/main/java/com/juliusmeinl/backend/model/Drzava.java
@@ -1,6 +1,7 @@
 package com.juliusmeinl.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -22,7 +23,7 @@ public class Drzava {
     private String nazivDrzave;
 
     @ToString.Exclude
-    @JsonIgnore
+    @JsonIgnoreProperties({"mjesta"})
     @OneToMany(mappedBy = "drzava")
     private List<Mjesto> mjesta;
 }
