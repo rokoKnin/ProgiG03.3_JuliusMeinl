a646896fce0e09f37e275bf1367597c2a7e22387
Nino Ljubas <nino.ljubas@gmail.com>
2026-01-19 17:56:04 +0100

    dodano upravljanje recenzijama

--- backend/src/main/java/com/juliusmeinl/backend/service/RecenzijaService.java
+++ backend/src/main/java/com/juliusmeinl/backend/service/RecenzijaService.java
@@ -6,22 +6,24 @@ import com.juliusmeinl.backend.model.Recenzija;
 import com.juliusmeinl.backend.repository.KorisnikRepository;
 import com.juliusmeinl.backend.repository.RecenzijaRepository;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Service
+@RequiredArgsConstructor
 public class RecenzijaService {
 
     private final RecenzijaRepository recenzijaRepository;
     private final KorisnikRepository korisnikRepository;
 
-    public RecenzijaService(RecenzijaRepository recenzijaRepository, KorisnikRepository korisnikRepository) {
-        this.recenzijaRepository = recenzijaRepository;
-        this.korisnikRepository = korisnikRepository;
-    }
+    private Random random = new Random();
 
     public Recenzija spremiRecenziju(String email, RecenzijaRequestDTO recenzijaDTO) {
         Korisnik korisnik = korisnikRepository.findByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, ""));
@@ -33,5 +35,19 @@ public class RecenzijaService {
         recenzija.setKomentar(recenzijaDTO.getKomentar());
 
        return recenzijaRepository.save(recenzija);
+    }
+
+    public List<Recenzija> generateRandom() {
+        List<Recenzija> recenzije = recenzijaRepository.findAll();
+
+        if (recenzije.size() <= 5) {
+            return recenzije;
+        } else {
+            List<Recenzija> randomRecenzije = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                randomRecenzije.add(recenzije.get(random.nextInt(recenzije.size())));
+            }
+            return randomRecenzije;
+        }
     }
 }
