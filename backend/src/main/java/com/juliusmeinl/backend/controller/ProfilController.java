9b733d7e3f4c3e3bc4b1bd2b103cb84797d166a8
Roko Tojčić <roko.tojcic@fer.hr>
2026-01-19 21:08:16 +0100

    smanjen kod na dohvat svih rezervacija

--- backend/src/main/java/com/juliusmeinl/backend/controller/ProfilController.java
+++ backend/src/main/java/com/juliusmeinl/backend/controller/ProfilController.java
@@ -33,9 +33,9 @@ public class ProfilController {
         return korisnikService.izmjeniKorisnika(korisnikRequestDTO);
     }
 
-    @GetMapping("/reservationsPassed/{korisnik_email}")
+    @GetMapping("/reservations/{korisnik_email}")
     public ResponseEntity<List<ProfilRezervacijeResponseDTO>> prosleRezervacije(@PathVariable String korisnik_email) {
-        return new ResponseEntity<>(rezervacijaService.dohvatiProsleRezervacijen(korisnik_email), HttpStatus.OK);
+        return new ResponseEntity<>(rezervacijaService.dohvatiRezervacije(korisnik_email), HttpStatus.OK);
     }
 
 }
