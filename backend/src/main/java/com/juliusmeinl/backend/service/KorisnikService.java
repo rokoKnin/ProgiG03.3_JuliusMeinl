8e289f14f7f3ae4608ec6c7f950a9873d283d820
Roko Tojčić <roko.tojcic@fer.hr>
2026-01-20 17:13:59 +0100

    Revert "promjena za ispis imena grada na profilu"
    This reverts commit 38e3b640a66b071584195d4cf34f92a7e0b58f1c.

--- backend/src/main/java/com/juliusmeinl/backend/service/KorisnikService.java
+++ backend/src/main/java/com/juliusmeinl/backend/service/KorisnikService.java
@@ -51,8 +51,8 @@ public class KorisnikService {
        korisnikResponseDTO.setPrezime(korisnik.getPrezime());
        korisnikResponseDTO.setEmail(korisnik.getEmail());
        korisnikResponseDTO.setTelefon(korisnik.getTelefon());
-       korisnikResponseDTO.setDrzava(korisnik.getMjesto().getDrzava().getNazivDrzave());
-       korisnikResponseDTO.setGrad(korisnik.getMjesto().getNazMjesto().substring(0,1).toUpperCase() + korisnik.getMjesto().getNazMjesto().substring(1));
+       korisnikResponseDTO.setDrzava(korisnik.getMjesto().getDrzava().getNazivDrzave().substring(0,1).toUpperCase() + korisnik.getMjesto().getDrzava().getNazivDrzave().substring(1));
+       korisnikResponseDTO.setGrad(korisnik.getMjesto().getNazMjesto());
        korisnikResponseDTO.setPostBr(korisnik.getMjesto().getPostBr());
 
        return korisnikResponseDTO;
